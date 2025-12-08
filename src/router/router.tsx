af4f9893763788634c6f7f552a4a83187605c1c7
qpaseo <qazcgik@gmail.com>
2025-12-08 19:27:39 +0900

    init :: 기초 파일에서 사용하지 않는 부분 임시적으로 삭제

--- src/router/router.tsx
+++ src/router/router.tsx
@@ -1,25 +1,18 @@
 import { Routes, Route } from "react-router-dom";
 import Login from "../page/login";
-import Signup from "../page/signup";
 import Main from "../page/main";
-import { ContestDetailPage } from "../page/contestDetail";
 import { ContestPage } from "../page/contests/index";
 import Profile from "../page/profile";
-import SolvePage from "../page/solve";
 import Problems from "../page/problems";
 
 export default function AppRoutes() {
   return (
     <Routes>
       <Route path="/login" element={<Login />} />
-      <Route path="/signup" element={<Signup />} />
       <Route path="/" element={<Main />} />
       <Route path="/profile" element={<Profile />} />
-      <Route path="/solve" element={<SolvePage />} />
-      <Route path="/solve/:problemId" element={<SolvePage />} />
       <Route path="/problems" element={<Problems />} />
       <Route path="/contests" element={<ContestPage />} />
-      <Route path="/contests/:contestId" element={<ContestDetailPage />} />
     </Routes>
   );
 }
