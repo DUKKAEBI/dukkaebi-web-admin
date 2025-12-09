6f0f7fd6c5def34fa71212cb21fadd8dfd408e42
qpaseo <qazcgik@gmail.com>
2025-12-09 16:52:34 +0900

    chore :: 유저 부분에서 수정된 라우터 파일 충돌 해결

--- src/router/router.tsx
+++ src/router/router.tsx
@@ -2,11 +2,7 @@ import { Routes, Route } from "react-router-dom";
 import Login from "../page/login";
 import Main from "../page/main";
 import ContestsPage from "../page/contests/index";
-import ContestCreate from "../page/contests/create";
-import ContestUpdate from "../page/contests/update";
-import ContestInfo from "../page/contests/info";
 import Profile from "../page/profile";
-import ProblemCreate from "../page/contests/problems/create";
 import Problems from "../page/problems";
 import UsersPage from "../page/users";
 
@@ -16,17 +12,9 @@ export default function AppRoutes() {
       <Route path="/login" element={<Login />} />
       <Route path="/" element={<Main />} />
       <Route path="/problems" element={<Problems />} />
-<<<<<<< HEAD
-      <Route path="/contests" element={<ContestsPage />} />
-      <Route path="/contests/create" element={<ContestCreate />} />
-      <Route path="/contests/update/:id" element={<ContestUpdate />} />
-      <Route path="/contests/info/:id" element={<ContestInfo />} />
-      <Route path="/contests/problems/create" element={<ProblemCreate />} />
-=======
       <Route path="/users" element={<UsersPage />} />
       <Route path="/user/:userId" element={<Profile />} />
-      <Route path="/contests" element={<ContestPage />} />
->>>>>>> 01bd294484d8b1367af14170444583e591756039
+      <Route path="/contests" element={<ContestsPage />} />
     </Routes>
   );
 }
